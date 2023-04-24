8fcbc375df350f59caae94c2d5d3a2249089e473
airmonitor <tomasz.szuster@gmail.com>
2023-04-24 12:27:44 +0200

    :bookmark: s3_service.py

--- aws_hexagonal_adapters/s3_service.py
+++ aws_hexagonal_adapters/s3_service.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """Library to simplify working with S3."""
-import boto3
-from botocore.config import Config
 import os
+from typing import Any, Optional, List
 
-# noinspection PyPackageRequirements
+import boto3
 from aws_lambda_powertools import Logger
+from botocore.config import Config
 from botocore.exceptions import ClientError
 
 LOGGER = Logger(sampling_rate=float(os.environ["LOG_SAMPLING_RATE"]), level=os.environ["LOG_LEVEL"])
@@ -23,8 +23,15 @@ class S3Service:
             "s3", region_name=region_name, config=Config(retries={"max_attempts": 10, "mode": "adaptive"})
         )
 
-    def upload(self, bucket, local_path, remote_path, extra_args=None):
-        """Upload files from local path to target path on S3 bucket."""
+    def upload(self, *, bucket: str, local_path: str, remote_path: str, extra_args=None) -> None:
+        """Upload files from local path to target path on S3 bucket.
+
+        :param bucket: S3 bucket name
+        :param local_path: local file path
+        :param remote_path: target file path
+        :param extra_args: extra arguments for upload
+        :return:
+        """
         if extra_args is None:
             extra_args = {}
         try:
@@ -34,8 +41,14 @@ class S3Service:
             LOGGER.error(f"Failed to upload file {local_path} into s3://{bucket}/{remote_path}")
             raise
 
-    def download(self, bucket, local_path, remote_path):
-        """Download files from path in S3 bucket to local path."""
+    def download(self, *, bucket: str, local_path: str, remote_path: str) -> None:
+        """Download files from path in S3 bucket to local path.
+
+        :param bucket: S3 bucket name
+        :param local_path: local file path
+        :param remote_path: target file path
+        :return:
+        """
         try:
             self.__s3.download_file(bucket, remote_path, local_path)
             LOGGER.info(f"Downloaded file s3://{bucket}/{remote_path} into {local_path}")
@@ -43,11 +56,17 @@ class S3Service:
             LOGGER.error("Failed to download file s3://{bucket}/{remote_path} into {local_path}")
             raise
 
-    def list_files(self, bucket, prefix, page_size=1000):
-        """List files using prefix path in S3 bucket."""
+    def list_files(self, *, bucket: str, prefix: str, page_size=1000) -> List[Optional[Any]]:
+        """List files using prefix path in S3 bucket.
+
+        :param bucket: S3 bucket name
+        :param prefix: prefix path
+        :param page_size: number of files to return per page
+        :return:
+        """
         try:
             token = None
-            files = []
+            files: list[str] = []
             while True:
                 if token is None:
                     response = self.__s3.list_objects_v2(Bucket=bucket, Prefix=prefix, MaxKeys=page_size)
@@ -70,11 +89,18 @@ class S3Service:
             LOGGER.error(f"Failed to list files in bucket {bucket} with prefix {prefix}")
             raise
 
-    def list_prefixes(self, bucket, delimiter, path_prefix="", page_size=1000):
-        """List prefixes that are in path prefix in S3 bucket."""
+    def list_prefixes(self, *, bucket, delimiter, path_prefix="", page_size=1000) -> list[Any | None]:
+        """List prefixes that are in path prefix in S3 bucket.
+
+        :param bucket: S3 bucket name
+        :param delimiter: delimiter
+        :param path_prefix: prefix path
+        :param page_size: number of files to return per page
+        :return:
+        """
         try:
             token = None
-            prefixes = []
+            prefixes: list[str] = []
             while True:
                 if token is None:
                     response = self.__s3.list_objects_v2(
@@ -107,8 +133,13 @@ class S3Service:
             LOGGER.error(f"Failed to list prefixes in bucket {bucket} with delimiter {delimiter}")
             raise
 
-    def delete_object(self, bucket, key):
-        """Delete file from S3 bucket."""
+    def delete_object(self, *, bucket: str, key: str) -> None:
+        """Delete file from S3 bucket.
+
+        :param bucket: S3 bucket name
+        :param key: file key
+        :return:
+        """
         try:
             self.__s3.delete_object(Bucket=bucket, Key=key)
             LOGGER.info(f"Deleted file s3://{bucket}/{key}")
@@ -118,8 +149,12 @@ class S3Service:
             LOGGER.error(f"Failed to delete file s3://{bucket}/{key}")
             raise
 
-    def delete_objects(self, bucket, keys):
-        """Delete files from S3 bucket."""
+    def delete_objects(self, *, bucket: str, keys: List[Optional[Any]]):
+        """Delete files from S3 bucket.
+
+        :param bucket: S3 bucket name
+        :param keys: list of S3 objects to be removed
+        """
         try:
             for idx in range(0, len(keys), 1000):
                 objects = [{"Key": key} for key in keys[idx : idx + 1000]]
@@ -129,14 +164,18 @@ class S3Service:
             LOGGER.error(f"Failed to delete objects from bucket {bucket}")
             raise
 
-    def delete_prefix(self, bucket, prefix):
-        """Delete all files from S3 bucket that have the same prefix."""
+    def delete_prefix(self, *, bucket: str, prefix: str):
+        """Delete all files from S3 bucket that have the same prefix.
+
+        :param bucket: S3 bucket name
+        :param prefix: prefix path
+        """
         LOGGER.info(f"Deleting prefix {prefix} from bucket {bucket}")
-        keys = self.list_files(bucket, prefix)
-        self.delete_objects(bucket, keys)
+        keys = self.list_files(bucket=bucket, prefix=prefix)
+        self.delete_objects(bucket=bucket, keys=keys)
         LOGGER.info(f"Deleted prefix {prefix} from bucket {bucket}")
 
-    def copy(self, source_bucket, source_key, target_bucket, target_key):
+    def copy(self, *, source_bucket: str, source_key: str, target_bucket: str, target_key: str):
         # sourcery skip: raise-specific-error
         """Copy file from S3 bucket to another S3 location."""
         try:
@@ -152,11 +191,20 @@ class S3Service:
             LOGGER.critical(f"Unexpected error in download_object function of s3 helper: {error}")
             raise Exception(f"Unexpected error in download_object function of s3 helper: {error}") from error
 
-    def move_object(self, source_bucket, source_key, target_bucket, target_key):
-        """Move file from S3 bucket to another S3 location."""
+    def move_object(self, *, source_bucket: str, source_key: str, target_bucket: str, target_key: str):
+        """Move file from S3 bucket to another S3 location.
+
+        :param source_bucket: S3 bucket name
+        :param source_key: file key
+        :param target_bucket: S3 bucket name
+        :param target_key: file key
+        :return:
+        """
         try:
-            self.copy(source_bucket, source_key, target_bucket, target_key)
-            self.delete_object(source_bucket, source_key)
+            self.copy(
+                source_bucket=source_bucket, source_key=source_key, target_bucket=target_bucket, target_key=target_key
+            )
+            self.delete_object(bucket=source_bucket, key=source_key)
             LOGGER.info(f"Object moved s3://{source_bucket}/{source_key} -> s3://{target_bucket}/{target_key}")
         except ClientError:
             LOGGER.error(
@@ -164,8 +212,15 @@ class S3Service:
             )
             raise
 
-    def move_objects(self, source_bucket, source_prefix, target_bucket, target_prefix):
-        """Move files from S3 bucket to another S3 location."""
+    def move_objects(self, *, source_bucket: str, source_prefix: str, target_bucket: str, target_prefix: str):
+        """Move files from S3 bucket to another S3 location.
+
+        :param  source_bucket: S3 bucket name
+        :param source_prefix: prefix path
+        :param target_bucket: S3 bucket name
+        :param target_prefix: prefix path
+        :return:
+        """
         try:
             LOGGER.info(
                 f"Starting moving objects s3://{source_bucket}/{source_prefix} -> s3://{target_bucket}/{target_prefix}",
@@ -186,7 +241,12 @@ class S3Service:
                         if file["Key"] == source_prefix:
                             continue
                         target_key = f"{file['Key'].replace(source_prefix, target_prefix)}"
-                        self.move_object(source_bucket, file["Key"], target_bucket, target_key)
+                        self.move_object(
+                            source_bucket=source_bucket,
+                            source_key=file["Key"],
+                            target_bucket=target_bucket,
+                            target_key=target_key,
+                        )
                 if response["IsTruncated"]:
                     token = response["NextContinuationToken"]
                 else:
